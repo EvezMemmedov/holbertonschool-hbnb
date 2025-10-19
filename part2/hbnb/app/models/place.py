464a371e43877df58aa3946260c117dde80afa34
EvezMemmedov <evezm254@gmail.com>
2025-10-19 18:17:18 +0000

    Added

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -6,26 +6,62 @@ class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
 
-    if not title or len(title) > 100:
-        raise ValueError("Title is required and must be <= 100 characters.")
-    if price <= 0:
-        raise ValueError("Prise must be a positive number.")
-    if not (-90.0 <= latitude <= 90.0):
-        raise ValueError("Latitude must be betwen -90.0 and 90.0.")
-    if not (-180.0 <= longitude <= 180.0):
-        raise ValueError("Longitude must be between -180.0 and 180.0.")
+        if not title or len(title) > 100:
+            raise ValueError("Title is required and must be <= 100 characters.")
+        if price < 0:
+            raise ValueError("Prise must be a positive number.")
+        if not (-90.0 <= latitude <= 90.0):
+            raise ValueError("Latitude must be betwen -90.0 and 90.0.")
+        if not (-180.0 <= longitude <= 180.0):
+            raise ValueError("Longitude must be between -180.0 and 180.0.")
 
-    self.title = title
-    self.description = description
-    self.price = price
-    self.latitude = latitude
-    self.longitude = longitude
-    self.owner = owner
-    self.reviews = []
-    self.amenities = []
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner_id = owner_id
+        self.reviews = []
+        self.amenities = []
 
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price must be a non-negative number.")
+        self._price = value
+    @property
+    def latitude(self):
+        return self._latitude
+    @latitude.setter
+    def latitude(self, value):
+        if not (-90.0 <= value <= 90.0):
+            raise ValueError("Latitude must be between -90.0 and 90.0.")
+        self._latitude = value
+    @property
+    def longitude(self):
+        return self._longitude
+    @longitude.setter
+    def longitude(self, value):
+        if not (-180.0 <= value <= 180.0):
+            raise ValueError("Longitude must be between -180.0 and 180.0.")
+        self._longitude = value
     def add_review(self, review):
         self.reviews.append(review)
 
     def add_amenity(self, amenity):
         self.amenities.append(amenity)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "price": self.price,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "owner_id": self.owner_id,
+            "amenities": self.amenities,
+        }
